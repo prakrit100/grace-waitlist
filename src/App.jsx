5fea258a78a4a64f2bd26c5d281de26653702a4f
prakrit100 <prakritojha441@gmail.com>
2024-10-04 09:27:53 +0545

    text-transition error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
-import TextTransition, { presets } from 'react-text-transition';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import dashboardImage from './assets/dashboard.jpeg';
